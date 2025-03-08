a1faac19c77ca4863855353f0c6ae231d8845463
DanDuhon <49920618+DanDuhon@users.noreply.github.com>
2025-03-08 10:13:13 -0600

    Sort the encounters dict ahead of time.

--- encounters_stitch.py
+++ encounters_stitch.py
@@ -20,5 +20,17 @@ for encounterFile in listdir(baseFolder + "\\combine"):
         }
     encounters[e["name"]]["expansionCombos"][int(encounterFile[-6])] = e["expansionCombos"]
 
+# Sort the dict so we can just load it in and not sort it later.
+coreSets = {"Painted World of Ariamis", "The Sunless City", "Tomb of Giants", "Dark Souls The Board Game"}
+v2Expansions = {"Painted World of Ariamis", "The Sunless City", "Tomb of Giants"}
+encounters = dict(sorted(encounters.items(), key=lambda x: (
+    1 if encounters[x]["level"] == 4 else 0,
+    0 if encounters[x]["expansion"] in coreSets and encounters[x]["expansion"] in v2Expansions else 1,
+    0 if encounters[x]["expansion"] in v2Expansions else 1,
+    1 if encounters[x]["expansion"] == "Executioner Chariot" else 0,
+    encounters[x]["expansion"],
+    encounters[x]["level"],
+    encounters[x]["name"])))
+
 with open(baseFolder + "\\dsbg_shuffle_encounters.json", "w") as efd:
     dump(encounters, efd)
